28f22a3dd9fedcfd13e4ec4470607c8e1657d093
mattzahel <mateusz.zahel@gmail.com>
2019-06-21 13:27:29 +0200

    basic home page design

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class="grey lighten-4">
     <Navbar />
-    <v-content class="ma-4">
+    <v-content>
       <router-view></router-view>
     </v-content>
     <Footer />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="home">
-    <h1>This is home page</h1>
+  <div class="home ma-0">
+    <v-parallax
+      height="600"
+      :src="require('@/assets/hero-bg.jpg')"
+      alt="parallax-image"
+      class="hidden-md-and-down parallax"
+    >
+      <v-layout align-center column justify-center>
+        <h1 class="display-3 font-weight-thin mb-3">Breaking Bad Info App</h1>
+        <h2 class="display-1">
+          Learn more about your favorite characters and find their most famous
+          quotes!
+        </h2>
+      </v-layout>
+    </v-parallax>
+    <div class="hidden-lg-and-up">
+      <v-img :src="require('@/assets/hero-bg.jpg')" alt="hero-image"></v-img>
+      <v-layout align-center column justify-center py-3 px-2>
+        <h1 class="headline mb-3">Breaking Bad Info App</h1>
+        <h2 class="title font-weight-thin">
+          Learn more about your favorite characters and find their most famous
+          quotes!
+        </h2>
+      </v-layout>
+    </div>
   </div>
 </template>
 
@@ -9,3 +32,4 @@ export default {
   components: {}
 }
 </script>
+<style lang="scss" scoped></style>
